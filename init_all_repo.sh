26ce7a7e284b82f1ef61f97a0cb7831eb2e33887
mortyL <mortyl0834@gmail.com>
2023-03-03 19:42:30 +0800

    [Fix][other]:Fix commit bug

--- init_all_repo.sh
+++ init_all_repo.sh
@@ -2,7 +2,7 @@
 #Init all functions in developing
 git submodule update --init --recursive
 
-git clone git@github.com:Peize-Liu/CommitFormatTest.git
-cp ./CommitFormatTest/prepare-commit-msg ./.git/hooks
-cp ./CommitFormatTest/prepare-commit-msg ./.git/modules/NxtPX4-Hardware/hooks
-cp ./CommitFormatTest/prepare-commit-msg ./.git/modules/NxtPX4-Hardware/hooks
+# git clone git@github.com:Peize-Liu/CommitFormatTest.git
+cp ./prepare-commit-msg ./.git/hooks
+cp ./prepare-commit-msg ./.git/modules/NxtPX4-Hardware/hooks
+cp ./prepare-commit-msg ./.git/modules/NxtPX4-Hardware/hooks
