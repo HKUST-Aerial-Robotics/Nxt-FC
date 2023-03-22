fd302877a10272fee2bd2ddb7c006302bf101d24
mortyL <mortyl0834@gmail.com>
2023-03-23 00:51:47 +0800

    [Doc][other]:caution infomation update

--- init_all_repo.sh
+++ init_all_repo.sh
@@ -4,5 +4,5 @@ git submodule update --init --recursive
 
 # git clone git@github.com:Peize-Liu/CommitFormatTest.git
 cp ./prepare-commit-msg ./.git/hooks
-cp ./prepare-commit-msg ./.git/modules/NxtPX4-Hardware/hooks
+cp ./prepare-commit-msg ./.git/modules/PX4-Autopilot/hooks
 cp ./prepare-commit-msg ./.git/modules/NxtPX4-Hardware/hooks
